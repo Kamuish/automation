c874d0e1d43cadb2a8de3b6058e57a3cc625822b
Kamuish <33263706+Kamuish@users.noreply.github.com>
2018-12-30 01:11:49 +0000

    Notifications of result

--- reddit_search/main.py
+++ reddit_search/main.py
@@ -1,3 +1,27 @@
 import praw
+from queries import start_connection, find_match
+from db import get_all_subs
+
+from win10toast import ToastNotifier
 
 
+
+
+def main():
+	reddit_instance = start_connection()
+
+	subs = get_all_subs()
+	matches = []
+
+	for subreddit in subs:
+		result = find_match(reddit_instance,subreddit[1], subreddit[2])
+		result = 0
+		if 0 == result:
+			matches.append(subreddit[1])
+
+	toaster = ToastNotifier()
+	toaster.show_toast("Things to read/see:",
+	                   "\n".join(matches),
+	                   duration=10)
+
+main()
